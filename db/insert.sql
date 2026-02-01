295889bb8b5fe4db264a163a8bb4c557cd3207a3
magda3424121 <magdalena.saveva@scv.si>
2026-02-01 20:48:45 +0100

    init: project setup with db scripts

---
+++ db/insert.sql
@@ -0,0 +1,522 @@
+INSERT INTO kraji (ime, postna_st)
+SELECT 'Adlešiči', 8341 UNION ALL
+SELECT 'Ajdovščina', 5270 UNION ALL
+SELECT 'Ankaran - Ancarano', 6280 UNION ALL
+SELECT 'Apače', 9253 UNION ALL
+SELECT 'Artiče', 8253 UNION ALL
+SELECT 'Bakovci', 9231 UNION ALL
+SELECT 'Banja Loka', 1336 UNION ALL
+SELECT 'Beltinci', 9231 UNION ALL
+SELECT 'Benedikt', 2234 UNION ALL
+SELECT 'Bistrica ob Dravi', 2345 UNION ALL
+SELECT 'Bistrica ob Sotli', 3256 UNION ALL
+SELECT 'Bizeljsko', 8259 UNION ALL
+SELECT 'Blagovica', 1223 UNION ALL
+SELECT 'Blanca', 8283 UNION ALL
+SELECT 'Bled', 4260 UNION ALL
+SELECT 'Blejska Dobrava', 4273 UNION ALL
+SELECT 'Bodonci', 9265 UNION ALL
+SELECT 'Bogojina', 9222 UNION ALL
+SELECT 'Bohinjska Bela', 4263 UNION ALL
+SELECT 'Bohinjska Bistrica', 4264 UNION ALL
+SELECT 'Bohinjsko Jezero', 4265 UNION ALL
+SELECT 'Borovnica', 1353 UNION ALL
+SELECT 'Boštanj', 8294 UNION ALL
+SELECT 'Bovec', 5230 UNION ALL
+SELECT 'Branik', 5295 UNION ALL
+SELECT 'Braslovče', 3314 UNION ALL
+SELECT 'Brestanica', 8280 UNION ALL
+SELECT 'Bresternica', 2354 UNION ALL
+SELECT 'Brezovica pri Ljubljani', 1351 UNION ALL
+SELECT 'Brežice', 8250 UNION ALL
+SELECT 'Brtonigla', 52474 UNION ALL
+SELECT 'Brusnice', 8321 UNION ALL
+SELECT 'Bučka', 8276 UNION ALL
+SELECT 'Buče', 3255 UNION ALL
+SELECT 'Budanje', 5271 UNION ALL
+SELECT 'Buje', 52460 UNION ALL
+SELECT 'Cankova', 9261 UNION ALL
+SELECT 'Celje', 3000 UNION ALL
+SELECT 'Cerklje na Gorenjskem', 4207 UNION ALL
+SELECT 'Cerklje ob Krki', 8263 UNION ALL
+SELECT 'Cerknica', 1380 UNION ALL
+SELECT 'Cerkno', 5282 UNION ALL
+SELECT 'Cerkvenjak', 2236 UNION ALL
+SELECT 'Cirkulane', 2282 UNION ALL
+SELECT 'Col', 5273 UNION ALL
+SELECT 'Čadrg', 5220 UNION ALL
+SELECT 'Črenšovci', 9232 UNION ALL
+SELECT 'Črna na Koroškem', 2393 UNION ALL
+SELECT 'Črni Kal', 6275 UNION ALL
+SELECT 'Črni Vrh nad Idrijo', 5274 UNION ALL
+SELECT 'Črnomelj', 8340 UNION ALL
+SELECT 'Deskle', 5210 UNION ALL
+SELECT 'Destrnik', 2253 UNION ALL
+SELECT 'Divača', 6215 UNION ALL
+SELECT 'Dob', 1233 UNION ALL
+SELECT 'Dobe', 8311 UNION ALL
+SELECT 'Dobova', 8257 UNION ALL
+SELECT 'Dobovica', 1273 UNION ALL
+SELECT 'Dobravlje', 5263 UNION ALL
+SELECT 'Dobrena', 2221 UNION ALL
+SELECT 'Dobrova', 1356 UNION ALL
+SELECT 'Dobrovnik/Dobronak', 9223 UNION ALL
+SELECT 'Dobrunje', 1261 UNION ALL
+SELECT 'Dole pri Litiji', 1273 UNION ALL
+SELECT 'Dolenja vas', 1331 UNION ALL
+SELECT 'Dolenjske Toplice', 8350 UNION ALL
+SELECT 'Domžale', 1230 UNION ALL
+SELECT 'Dornava', 2252 UNION ALL
+SELECT 'Dornberk', 5294 UNION ALL
+SELECT 'Draga', 1319 UNION ALL
+SELECT 'Dragomer', 1351 UNION ALL
+SELECT 'Dragočajna', 1216 UNION ALL
+SELECT 'Dramlje', 3222 UNION ALL
+SELECT 'Dravograd', 2370 UNION ALL
+SELECT 'Dutovlje', 6221 UNION ALL
+SELECT 'Dvor', 8302 UNION ALL
+SELECT 'Fram', 2327 UNION ALL
+SELECT 'Frankolovo', 3213 UNION ALL
+SELECT 'Gabrovka', 1274 UNION ALL
+SELECT 'Gabrje', 8321 UNION ALL
+SELECT 'Gorenja vas', 4224 UNION ALL
+SELECT 'Gorišnica', 2272 UNION ALL
+SELECT 'Gornja Radgona', 9250 UNION ALL
+SELECT 'Gornja Težka Voda', 8322 UNION ALL
+SELECT 'Gornji Grad', 3342 UNION ALL
+SELECT 'Gozd Martuljek', 4282 UNION ALL
+SELECT 'Gračišče', 6272 UNION ALL
+SELECT 'Grad', 9264 UNION ALL
+SELECT 'Gradac', 8332 UNION ALL
+SELECT 'Grahovo', 1384 UNION ALL
+SELECT 'Grahovo ob Bači', 5242 UNION ALL
+SELECT 'Grebenje', 1336 UNION ALL
+SELECT 'Griže', 3302 UNION ALL
+SELECT 'Grosuplje', 1290 UNION ALL
+SELECT 'Hajdina', 2288 UNION ALL
+SELECT 'Hinje', 8362 UNION ALL
+SELECT 'Hodoš/Hodos', 9205 UNION ALL
+SELECT 'Horjul', 1354 UNION ALL
+SELECT 'Hotič', 1270 UNION ALL
+SELECT 'Hotedršica', 1372 UNION ALL
+SELECT 'Hrastnik', 1430 UNION ALL
+SELECT 'Hrpelje', 6240 UNION ALL
+SELECT 'Idrija', 5280 UNION ALL
+SELECT 'Ig', 1292 UNION ALL
+SELECT 'Ilirska Bistrica', 6250 UNION ALL
+SELECT 'Ivančna Gorica', 1295 UNION ALL
+SELECT 'Ivanjkovci', 2259 UNION ALL
+SELECT 'Izlake', 1411 UNION ALL
+SELECT 'Izola/Isola', 6310 UNION ALL
+SELECT 'Jakobski Dol', 2222 UNION ALL
+SELECT 'Jarenina', 2221 UNION ALL
+SELECT 'Jelšane', 6254 UNION ALL
+SELECT 'Jesenice', 4270 UNION ALL
+SELECT 'Jesenice na Dolenjskem', 8261 UNION ALL
+SELECT 'Juršinci', 2256 UNION ALL
+SELECT 'Kal nad Kanalom', 5214 UNION ALL
+SELECT 'Kamnik', 1241 UNION ALL
+SELECT 'Kanal', 5213 UNION ALL
+SELECT 'Kapele', 8258 UNION ALL
+SELECT 'Kidričevo', 2325 UNION ALL
+SELECT 'Kisovec', 1412 UNION ALL
+SELECT 'Kobarid', 5222 UNION ALL
+SELECT 'Kobilje', 9227 UNION ALL
+SELECT 'Kočevje', 1330 UNION ALL
+SELECT 'Kočevska Reka', 1338 UNION ALL
+SELECT 'Kog', 2276 UNION ALL
+SELECT 'Kojsko', 5211 UNION ALL
+SELECT 'Komen', 6223 UNION ALL
+SELECT 'Komenda', 1218 UNION ALL
+SELECT 'Koper/Capodistria', 6000 UNION ALL
+SELECT 'Koprivnica', 8282 UNION ALL
+SELECT 'Kostanjevica na Krki', 8311 UNION ALL
+SELECT 'Kostanjevica na Krasu', 5296 UNION ALL
+SELECT 'Košana', 6257 UNION ALL
+SELECT 'Kotlje', 2394 UNION ALL
+SELECT 'Kozina', 6240 UNION ALL
+SELECT 'Kozje', 3260 UNION ALL
+SELECT 'Kranj', 4000 UNION ALL
+SELECT 'Kranjska Gora', 4280 UNION ALL
+SELECT 'Krapje', 9241 UNION ALL
+SELECT 'Kresnice', 1281 UNION ALL
+SELECT 'Križevci', 9206 UNION ALL
+SELECT 'Križevci pri Ljutomeru', 9242 UNION ALL
+SELECT 'Krka', 1295 UNION ALL
+SELECT 'Krmelj', 8296 UNION ALL
+SELECT 'Kropa', 4245 UNION ALL
+SELECT 'Krško', 8270 UNION ALL
+SELECT 'Kuzma', 9263 UNION ALL
+SELECT 'Laporje', 2318 UNION ALL
+SELECT 'Lasko', 3270 UNION ALL
+SELECT 'Laze v Tuhinju', 1219 UNION ALL
+SELECT 'Laznica', 2341 UNION ALL
+SELECT 'Lendava/Lendva', 9220 UNION ALL
+SELECT 'Lesce', 4248 UNION ALL
+SELECT 'Leskovec pri Krškem', 8273 UNION ALL
+SELECT 'Libeliče', 2372 UNION ALL
+SELECT 'Limbuš', 2341 UNION ALL
+SELECT 'Litija', 1270 UNION ALL
+SELECT 'Ljubečna', 3202 UNION ALL
+SELECT 'Ljubljana', 1000 UNION ALL
+SELECT 'Ljubljana-Polje', 1260 UNION ALL
+SELECT 'Ljubljana-Šmartno', 1211 UNION ALL
+SELECT 'Ljubno ob Savinji', 3333 UNION ALL
+SELECT 'Ljutomer', 9240 UNION ALL
+SELECT 'Loče', 3215 UNION ALL
+SELECT 'Log pod Mangartom', 5231 UNION ALL
+SELECT 'Logatec', 1370 UNION ALL
+SELECT 'Loka pri Zidanem Mostu', 1434 UNION ALL
+SELECT 'Lokev', 6219 UNION ALL
+SELECT 'Loški Potok', 1318 UNION ALL
+SELECT 'Lovrenc na Dravskem polju', 2324 UNION ALL
+SELECT 'Lovrenc na Pohorju', 2344 UNION ALL
+SELECT 'Loka pri Žusmu', 3223 UNION ALL
+SELECT 'Luče', 3334 UNION ALL
+SELECT 'Lukovica', 1225 UNION ALL
+SELECT 'Mačkovci', 9202 UNION ALL
+SELECT 'Majšperk', 2322 UNION ALL
+SELECT 'Makole', 2321 UNION ALL
+SELECT 'Mala Nedelja', 9243 UNION ALL
+SELECT 'Malečnik', 2229 UNION ALL
+SELECT 'Marezige', 6273 UNION ALL
+SELECT 'Maribor', 2000 UNION ALL
+SELECT 'Marija Reka', 3312 UNION ALL
+SELECT 'Markovci', 2281 UNION ALL
+SELECT 'Martjanci', 9221 UNION ALL
+SELECT 'Materija', 6242 UNION ALL
+SELECT 'Mavčiče', 1211 UNION ALL
+SELECT 'Medvode', 1215 UNION ALL
+SELECT 'Mengeš', 1234 UNION ALL
+SELECT 'Metlika', 8330 UNION ALL
+SELECT 'Mežica', 2392 UNION ALL
+SELECT 'Miklavž na Dravskem polju', 2204 UNION ALL
+SELECT 'Miklavž pri Ormožu', 2275 UNION ALL
+SELECT 'Miren', 5291 UNION ALL
+SELECT 'Mirna', 8233 UNION ALL
+SELECT 'Mirna Peč', 8216 UNION ALL
+SELECT 'Mislinja', 2382 UNION ALL
+SELECT 'Mojstrana', 4281 UNION ALL
+SELECT 'Mokronog', 8230 UNION ALL
+SELECT 'Moravče', 1251 UNION ALL
+SELECT 'Moravske Toplice', 9226 UNION ALL
+SELECT 'Most na Soči', 5216 UNION ALL
+SELECT 'Motnik', 1221 UNION ALL
+SELECT 'Mozirje', 3330 UNION ALL
+SELECT 'Murska Sobota', 9000 UNION ALL
+SELECT 'Muta', 2366 UNION ALL
+SELECT 'Naklo', 4202 UNION ALL
+SELECT 'Nazarje', 3331 UNION ALL
+SELECT 'Notranje Gorice', 1357 UNION ALL
+SELECT 'Nova Cerkev', 3203 UNION ALL
+SELECT 'Nova Gorica', 5000 UNION ALL
+SELECT 'Nova vas', 1385 UNION ALL
+SELECT 'Novo mesto', 8000 UNION ALL
+SELECT 'Obrov', 6243 UNION ALL
+SELECT 'Odranci', 9233 UNION ALL
+SELECT 'Oplotnica', 2317 UNION ALL
+SELECT 'Orehova vas', 2312 UNION ALL
+SELECT 'Ormož', 2270 UNION ALL
+SELECT 'Ortnek', 1316 UNION ALL
+SELECT 'Osilnica', 1337 UNION ALL
+SELECT 'Otočec', 8222 UNION ALL
+SELECT 'Ožbalt', 2361 UNION ALL
+SELECT 'Pameče', 2380 UNION ALL
+SELECT 'Pernica', 2231 UNION ALL
+SELECT 'Pesnica pri Mariboru', 2211 UNION ALL
+SELECT 'Petrovci', 9203 UNION ALL
+SELECT 'Petrovče', 3301 UNION ALL
+SELECT 'Piran/Pirano', 6330 UNION ALL
+SELECT 'Pišece', 8255 UNION ALL
+SELECT 'Pivka', 6257 UNION ALL
+SELECT 'Planina', 6232 UNION ALL
+SELECT 'Planina pri Sevnici', 3225 UNION ALL
+SELECT 'Pobegi', 6276 UNION ALL
+SELECT 'Podčetrtek', 3254 UNION ALL
+SELECT 'Podgorje', 6216 UNION ALL
+SELECT 'Podgorje pri Letušu', 3327 UNION ALL
+SELECT 'Podgrad', 6244 UNION ALL
+SELECT 'Podkum', 1414 UNION ALL
+SELECT 'Podlehnik', 2286 UNION ALL
+SELECT 'Podnanos', 5272 UNION ALL
+SELECT 'Podnart', 4244 UNION ALL
+SELECT 'Podplat', 3241 UNION ALL
+SELECT 'Podsreda', 3257 UNION ALL
+SELECT 'Podvelka', 2363 UNION ALL
+SELECT 'Pohorje', 2208 UNION ALL
+SELECT 'Poljane nad Škofjo Loko', 4223 UNION ALL
+SELECT 'Poljčane', 2319 UNION ALL
+SELECT 'Polšnik', 1272 UNION ALL
+SELECT 'Polzela', 3313 UNION ALL
+SELECT 'Ponikva', 3232 UNION ALL
+SELECT 'Portorož/Portorose', 6320 UNION ALL
+SELECT 'Postojna', 6230 UNION ALL
+SELECT 'Pragersko', 2331 UNION ALL
+SELECT 'Prebold', 3312 UNION ALL
+SELECT 'Preddvor', 4205 UNION ALL
+SELECT 'Prem', 6255 UNION ALL
+SELECT 'Preserje', 1352 UNION ALL
+SELECT 'Prestranek', 6258 UNION ALL
+SELECT 'Prevalje', 2391 UNION ALL
+SELECT 'Prežganje', 1261 UNION ALL
+SELECT 'Primskovo', 1276 UNION ALL
+SELECT 'Pristava pri Mestinju', 3253 UNION ALL
+SELECT 'Prosenjakovci/Partosfalva', 9207 UNION ALL
+SELECT 'Prvačina', 5297 UNION ALL
+SELECT 'Ptuj', 2250 UNION ALL
+SELECT 'Ptujska Gora', 2323 UNION ALL
+SELECT 'Puconci', 9201 UNION ALL
+SELECT 'Rače', 2327 UNION ALL
+SELECT 'Radeče', 1433 UNION ALL
+SELECT 'Radenci', 9252 UNION ALL
+SELECT 'Radlje ob Dravi', 2360 UNION ALL
+SELECT 'Radomlje', 1235 UNION ALL
+SELECT 'Radovljica', 4240 UNION ALL
+SELECT 'Raka', 8274 UNION ALL
+SELECT 'Rakek', 1381 UNION ALL
+SELECT 'Rateče-Planica', 4283 UNION ALL
+SELECT 'Ravne na Koroškem', 2390 UNION ALL
+SELECT 'Rečica ob Savinji', 3332 UNION ALL
+SELECT 'Renče', 5292 UNION ALL
+SELECT 'Ribnica', 1310 UNION ALL
+SELECT 'Ribnica na Pohorju', 2364 UNION ALL
+SELECT 'Rimske Toplice', 3272 UNION ALL
+SELECT 'Rob', 1314 UNION ALL
+SELECT 'Ročinj', 5213 UNION ALL
+SELECT 'Rogaška Slatina', 3250 UNION ALL
+SELECT 'Rogašovci', 9262 UNION ALL
+SELECT 'Rogatec', 3252 UNION ALL
+SELECT 'Rovte', 1373 UNION ALL
+SELECT 'Ruše', 2342 UNION ALL
+SELECT 'Sava', 1282 UNION ALL
+SELECT 'Sečovlje/Sicciole', 6333 UNION ALL
+SELECT 'Sedraž', 3270 UNION ALL
+SELECT 'Sela pri Šumberku', 8360 UNION ALL
+SELECT 'Selca', 4227 UNION ALL
+SELECT 'Selnica ob Dravi', 2352 UNION ALL
+SELECT 'Semič', 8333 UNION ALL
+SELECT 'Senovo', 8281 UNION ALL
+SELECT 'Senožeče', 6224 UNION ALL
+SELECT 'Sevnica', 8290 UNION ALL
+SELECT 'Sežana', 6210 UNION ALL
+SELECT 'Sladki Vrh', 2214 UNION ALL
+SELECT 'Slap ob Idrijci', 5283 UNION ALL
+SELECT 'Slovenj Gradec', 2380 UNION ALL
+SELECT 'Slovenska Bistrica', 2310 UNION ALL
+SELECT 'Slovenske Konjice', 3210 UNION ALL
+SELECT 'Smlednik', 1216 UNION ALL
+SELECT 'Sostro', 1261 UNION ALL
+SELECT 'Soča', 5232 UNION ALL
+SELECT 'Solčava', 3335 UNION ALL
+SELECT 'Solkan', 5250 UNION ALL
+SELECT 'Spodnja Idrija', 5281 UNION ALL
+SELECT 'Spodnja Kungota', 2201 UNION ALL
+SELECT 'Spodnje Kraše', 3331 UNION ALL
+SELECT 'Središče ob Dravi', 2277 UNION ALL
+SELECT 'Srednja vas v Bohinju', 4267 UNION ALL
+SELECT 'Sromlje', 8256 UNION ALL
+SELECT 'Srpenica', 5224 UNION ALL
+SELECT 'Stahovica', 1242 UNION ALL
+SELECT 'Stara Cerkev', 1332 UNION ALL
+SELECT 'Stari trg ob Kolpi', 8342 UNION ALL
+SELECT 'Stari trg pri Ložu', 1386 UNION ALL
+SELECT 'Starše', 2325 UNION ALL
+SELECT 'Stoperce', 2289 UNION ALL
+SELECT 'Stopiče', 8322 UNION ALL
+SELECT 'Stranice', 3206 UNION ALL
+SELECT 'Strunjan/Strugnano', 6320 UNION ALL
+SELECT 'Studenec', 8293 UNION ALL
+SELECT 'Suhor', 8331 UNION ALL
+SELECT 'Sv. Ana v Slovenskih goricah', 2233 UNION ALL
+SELECT 'Sv. Duh na Ostrem Vrhu', 2353 UNION ALL
+SELECT 'Sv. Jurij ob Ščavnici', 9244 UNION ALL
+SELECT 'Sv. Jurij v Slovenskih goricah', 2235 UNION ALL
+SELECT 'Sv. Trojica v Slovenskih goricah', 2235 UNION ALL
+SELECT 'Sveti Stefan', 3241 UNION ALL
+SELECT 'Sveti Tomaž', 2258 UNION ALL
+SELECT 'Šalovci', 9204 UNION ALL
+SELECT 'Šempas', 5261 UNION ALL
+SELECT 'Šempeter pri Gorici', 5290 UNION ALL
+SELECT 'Šempeter v Savinjski dolini', 3311 UNION ALL
+SELECT 'Šenčur', 4208 UNION ALL
+SELECT 'Šentilj v Slovenskih goricah', 2212 UNION ALL
+SELECT 'Šentjanž', 8297 UNION ALL
+SELECT 'Šentjanž pri Dravogradu', 2373 UNION ALL
+SELECT 'Šentjernej', 8310 UNION ALL
+SELECT 'Šentjur', 3230 UNION ALL
+SELECT 'Šentlovrenc', 8212 UNION ALL
+SELECT 'Šentvid pri Stični', 1296 UNION ALL
+SELECT 'Škocjan', 8275 UNION ALL
+SELECT 'Škofja Loka', 4220 UNION ALL
+SELECT 'Škofljica', 1291 UNION ALL
+SELECT 'Šmarje', 6274 UNION ALL
+SELECT 'Šmarje pri Jelšah', 3240 UNION ALL
+SELECT 'Šmarje-Sap', 1293 UNION ALL
+SELECT 'Šmarješke Toplice', 8222 UNION ALL
+SELECT 'Šmartno na Pohorju', 2315 UNION ALL
+SELECT 'Šmartno ob Dreti', 3341 UNION ALL
+SELECT 'Šmartno ob Paki', 3327 UNION ALL
+SELECT 'Šmartno pri Litiji', 1275 UNION ALL
+SELECT 'Šmartno pri Slovenj Gradcu', 2383 UNION ALL
+SELECT 'Šmartno v Rožni dolini', 3201 UNION ALL
+SELECT 'Šoštanj', 3325 UNION ALL
+SELECT 'Štanjel', 6222 UNION ALL
+SELECT 'Štore', 3220 UNION ALL
+SELECT 'Tabor', 3304 UNION ALL
+SELECT 'Teharje', 3221 UNION ALL
+SELECT 'Tišina', 9251 UNION ALL
+SELECT 'Tolmin', 5220 UNION ALL
+SELECT 'Topolšica', 3326 UNION ALL
+SELECT 'Trbovlje', 1420 UNION ALL
+SELECT 'Trebelno', 8231 UNION ALL
+SELECT 'Trebnje', 8210 UNION ALL
+SELECT 'Trnovo pri Gorici', 5252 UNION ALL
+SELECT 'Trnovska vas', 2254 UNION ALL
+SELECT 'Trojane', 1222 UNION ALL
+SELECT 'Trzin', 1236 UNION ALL
+SELECT 'Tržič', 4290 UNION ALL
+SELECT 'Tržišče', 8295 UNION ALL
+SELECT 'Turjak', 1311 UNION ALL
+SELECT 'Turnišče', 9224 UNION ALL
+SELECT 'Unec', 1381 UNION ALL
+SELECT 'Vaneča', 9201 UNION ALL
+SELECT 'Vas', 1336 UNION ALL
+SELECT 'Velenje', 3320 UNION ALL
+SELECT 'Velika Loka', 8212 UNION ALL
+SELECT 'Velika Nedelja', 2274 UNION ALL
+SELECT 'Velika Polana', 9225 UNION ALL
+SELECT 'Velike Lašče', 1315 UNION ALL
+SELECT 'Veržej', 9241 UNION ALL
+SELECT 'Videm pri Ptuju', 2284 UNION ALL
+SELECT 'Videm-Dobrepolje', 1312 UNION ALL
+SELECT 'Vignole', 6280 UNION ALL
+SELECT 'Vinan', 6274 UNION ALL
+SELECT 'Vipava', 5271 UNION ALL
+SELECT 'Visoko', 4212 UNION ALL
+SELECT 'Višnja Gora', 1294 UNION ALL
+SELECT 'Vitanje', 3205 UNION ALL
+SELECT 'Vitomarci', 2255 UNION ALL
+SELECT 'Vodice', 1217 UNION ALL
+SELECT 'Vojnik', 3212 UNION ALL
+SELECT 'Volčja Draga', 5293 UNION ALL
+SELECT 'Voličina', 2232 UNION ALL
+SELECT 'Vransko', 3305 UNION ALL
+SELECT 'Vrhnika', 1360 UNION ALL
+SELECT 'Vuhred', 2365 UNION ALL
+SELECT 'Vuzenica', 2367 UNION ALL
+SELECT 'Zabukovje', 8292 UNION ALL
+SELECT 'Zagorje ob Savi', 1410 UNION ALL
+SELECT 'Zasip', 4260 UNION ALL
+SELECT 'Zavrč', 2283 UNION ALL
+SELECT 'Zdole', 8272 UNION ALL
+SELECT 'Zgornja Besnica', 4201 UNION ALL
+SELECT 'Zgornja Kungota', 2201 UNION ALL
+SELECT 'Zgornja Ložnica', 2316 UNION ALL
+SELECT 'Zgornja Polskava', 2314 UNION ALL
+SELECT 'Zgornja Velka', 2213 UNION ALL
+SELECT 'Zgornje Gorje', 4247 UNION ALL
+SELECT 'Zgornje Jezersko', 4206 UNION ALL
+SELECT 'Zgornje Konjišče', 9250 UNION ALL
+SELECT 'Zidani Most', 1432 UNION ALL
+SELECT 'Zreče', 3214 UNION ALL
+SELECT 'Žalec', 3310 UNION ALL
+SELECT 'Železniki', 4228 UNION ALL
+SELECT 'Žetale', 2287 UNION ALL
+SELECT 'Žiri', 4226 UNION ALL
+SELECT 'Žirovnica', 4274 UNION ALL
+SELECT 'Žužemberk', 8360;
+
+INSERT INTO sportni_centri (ime,naslov,kontakt,k_id) VALUES
+('Fit Center Ljubljana','Dunajska 10',111111111,1),
+('Gym Maribor','Glavni trg 5',222222222,2),
+('Celje Sport','Mariborska 15',333333333,3),
+('Koper Fit','Obala 7',444444444,4),
+('Kranj Aktiv','Cesta 1',555555555,5),
+('Ptuj Gym','Dravska 2',666666666,6),
+('Novo Mesto Fit','Nov trg 3',777777777,7),
+('Velenje Sport','Kidričeva 4',888888888,8),
+('Nova Gorica Gym','Erjavčeva 6',999999999,9),
+('Murska Sobota Fit','Slovenska 8',101010101,10);
+
+INSERT INTO sportne_dvorane (ime,kapaciteta,sc_id) VALUES
+('Dvorana 1',50,1),
+('Dvorana 2',40,2),
+('Dvorana 3',30,3),
+('Dvorana 4',25,4),
+('Dvorana 5',60,5),
+('Dvorana 6',35,6),
+('Dvorana 7',45,7),
+('Dvorana 8',20,8),
+('Dvorana 9',55,9),
+('Dvorana 10',70,10);
+
+INSERT INTO uporabniki (ime,priimek,email,geslo,telefon,vrsta_uporabnika) VALUES
+('Ana','Novak','ana@test.com','123',111111111,'stranka'),
+('Miha','Kovač','miha@test.com','123',222222222,'stranka'),
+('Sara','Horvat','sara@test.com','123',333333333,'stranka'),
+('Luka','Zupan','luka@test.com','123',444444444,'admin'),
+('Eva','Kralj','eva@test.com','123',555555555,'stranka'),
+('Marko','Mlakar','marko@test.com','123',666666666,'stranka'),
+('Nina','Bizjak','nina@test.com','123',777777777,'stranka'),
+('Jan','Potočnik','jan@test.com','123',888888888,'stranka'),
+('Tina','Kovačič','tina@test.com','123',999999999,'stranka'),
+('Rok','Vidmar','rok@test.com','123',101010101,'admin');
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Nogomet','Rekreativni nogomet', sd_id FROM sportne_dvorane WHERE ime='Dvorana 1' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Kosarka','Ekipni sport', sd_id FROM sportne_dvorane WHERE ime='Dvorana 2' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Odbojka','Dvoranska odbojka', sd_id FROM sportne_dvorane WHERE ime='Dvorana 3' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Tenis','Sport z loparjem', sd_id FROM sportne_dvorane WHERE ime='Dvorana 4' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Fitnes','Vadba moci', sd_id FROM sportne_dvorane WHERE ime='Dvorana 5' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Joga','Sprostitvena vadba', sd_id FROM sportne_dvorane WHERE ime='Dvorana 6' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Rokomet','Ekipni sport', sd_id FROM sportne_dvorane WHERE ime='Dvorana 7' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Badminton','Igra z loparjem', sd_id FROM sportne_dvorane WHERE ime='Dvorana 8' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Rokomet','Ekipni sport', sd_id FROM sportne_dvorane WHERE ime='Dvorana 9' LIMIT 1;
+
+INSERT INTO sporti (ime, opis, sd_id)
+SELECT 'Namizni tenis','Ping pong', sd_id FROM sportne_dvorane WHERE ime='Dvorana 10' LIMIT 1;
+
+
+-- 1) odstrani foreign key (da lahko spremeniš tip)
+ALTER TABLE "sporti" DROP CONSTRAINT IF EXISTS "Relationship6";
+
+-- 2) spremeni tip FK stolpca na INT8
+ALTER TABLE "sporti" ALTER COLUMN "sd_id" SET DATA TYPE INT8;
+
+-- 3) (opcijsko, ampak priporočeno) tudi v dvoranah naj bo INT8
+-- v CockroachDB je sd_id pri SERIAL skoraj vedno že INT8, ampak to ne škodi:
+ALTER TABLE "sportne_dvorane" ALTER COLUMN "sd_id" SET DATA TYPE INT8;
+
+-- 4) dodaj foreign key nazaj
+ALTER TABLE "sporti"
+  ADD CONSTRAINT "Relationship6"
+  FOREIGN KEY ("sd_id")
+  REFERENCES "sportne_dvorane" ("sd_id");
+
+INSERT INTO nastavitve (barva_ozadja, barva_pisave, velikost_pisave) VALUES
+('white','black',12),
+('black','white',14),
+('lightgray','black',13),
+('darkblue','white',15),
+('beige','brown',12),
+('lightblue','navy',14),
+('lightgreen','darkgreen',13),
+('lavender','purple',12),
+('yellow','black',16),
+('pink','darkred',13);
