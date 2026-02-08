70dbfc0afed72abf11156f17c65537bf6ead87d6
magda3424121 <magdalena.saveva@scv.si>
2026-02-08 18:19:07 +0100

    feat: dodan health endpoint

---
+++ demo/demo/src/main/java/com/magdalena/rezervacije/HealthController.java
@@ -0,0 +1,13 @@
+package com.magdalena.rezervacije;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "OK";
+    }
+}
